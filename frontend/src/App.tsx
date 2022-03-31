2f492f75caf74901dfb645b31c079bd1ebaed869
McMastS <spencerm133@gmail.com>
2022-03-31 17:12:28 -0600

    Add button group to select which action to perform

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
 import AddKeyValueForm from './components/AddKeyValueForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 function App() {
   return (
@@ -12,6 +13,11 @@ function App() {
       <Row>
         <Col>
           <h1>Spencer's Key Value Store</h1>
+          <ButtonGroup aria-label="Basic example">
+            <Button variant="success">Add key/value pair</Button>
+            <Button variant="secondary">Retrieve value</Button>
+            <Button variant="danger">Delete key</Button>
+          </ButtonGroup>
           <AddKeyValueForm />
         </Col>
       </Row>
