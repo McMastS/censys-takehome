d22b6ca5b57ca10efca389f91683522d26324cdf
McMastS <spencerm133@gmail.com>
2022-03-31 16:45:59 -0600

    Initial frontend structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Spencer's Key Value Store</h1>
+      <input></input>
+    </div>
+  );
+}
+
+export default App;
