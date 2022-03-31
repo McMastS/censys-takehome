6b6ef7a1e39a19f22fb0b6aca358cbd7e28dc447
McMastS <spencerm133@gmail.com>
2022-03-31 17:06:10 -0600

    Add AddKeyValueForm

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import AddKeyValueForm from './components/AddKeyValueForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Spencer's Key Value Store</h1>
-      <input></input>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <h1>Spencer's Key Value Store</h1>
+          <AddKeyValueForm />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
